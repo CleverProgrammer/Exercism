d7fc747672c80be0bb1bc12e6831c91e65c0969c
QaziPython <rafehqazi1@gmail.com>
2016-05-14 02:02:50 -0500

    Refactor pangram

--- python/pangram/pangram.py
+++ python/pangram/pangram.py
@@ -1,4 +1,4 @@
 def is_pangram(sentence):
     alphabet = 'abcdefghijklmnopqrstuvwxyz'
     sentence = [char for char in sentence.lower() if char in alphabet]
-    return set(sentence) == set('abcdefghijklmnopqrstuvwxyz')
+    return set(sentence) == set(alphabet)
