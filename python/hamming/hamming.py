822dcd64bf00642da58dacf190a5e0e179d1480d
QaziPython <rafehqazi1@gmail.com>
2016-05-14 02:56:14 -0500

    Use zip

--- python/hamming/hamming.py
+++ python/hamming/hamming.py
@@ -1,6 +1,6 @@
 def distance(strand_1, strand_2):
     hamming_distance = 0
-    for index, nucleotide in enumerate(strand_1):
-        if strand_2[index] != nucleotide:
+    for nucleotide_1, nucleotide_2 in zip(strand_1, strand_2):
+        if nucleotide_1 != nucleotide_2:
             hamming_distance += 1
     return hamming_distance
